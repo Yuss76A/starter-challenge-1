778cfcca8e95979a9050b39b7a802d89e9f02c39
Yuss76A <yuse_91yus@hotmail.com>
2025-03-08 01:26:41 +0100

    Create a spinning loading icon

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -2,29 +2,53 @@ import React from "react";
 import css from "./css/Content.module.css";
 import {savedPosts} from "../posts.json";
 import PostItem from "./PostItem";
+import Loader from "./Loader";
 
 class Content extends React.Component {
     constructor(props) {
-        super(props);
+        super(props)
+        this.state = {
+            isLoaded: false,
+            posts: [],
+        }
+    }
+
+    componentDidMount() {
+        setTimeout(()=>{
+            this.setState({
+                isLoaded: true,
+                posts: savedPosts,
+            })
+        }, 2000)
     }
 
     render() {
         return (
-            <div>
-                <h1>My Posts</h1>
+            <div className={css.Content}>
+                
+                <div className={css.TitleBar}>
+                    <h1>My Photos</h1>
+                    <form>
+                        <label htmlFor='searchinput'>Search</label>
+                        <input 
+                        type='search' 
+                        id='searchinput' 
+                        placeholder='By Author'
+                        onChange={(e) => this.handleChange(e)}
+                        />
+                        <h4>posts found {this.state.posts.length}</h4>
+                    </form>
+                </div>
+
                 <div className={css.SearchResults}>
-                    {savedPosts.map((post, index) => {
-                        return (
-                            <div key={index} className={css.SearchItem}>
-                                {/* For testing purposes, replace with actual post content later */}
-                                <p>Individual Post</p>
-                            </div>
-                        );
-                    })}
+                    {
+                        this.state.isLoaded ?
+                        <PostItem savedPosts={this.state.posts} />
+                        : <Loader />
+                    }
                 </div>
             </div>
-        );
+        )
     }
 }
-
 export default Content
--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import css from "./css/Sidebar.module.css";
 
 function Sidebar() {
     return (
-        <div>
+        <div className={CSS.Sidebar}>
             <a href = "#." target= "_blank">My Photos</a>
             <a href = "#." target= "_blank">My Illustrations</a>
             <a href = "#." target= "_blank">My Paintings</a>
