d34dc39c6312ad4dbc9ec6e3fb3dc8a3ef1ecffc
Yuss76A <yuse_91yus@hotmail.com>
2025-03-10 15:24:07 +0100

    Network Requests Challenge part 3: Converting your ClassAPI to a functional component using Hooks

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,22 @@
-import css from './App.module.css';
+import css from "./App.module.css";
+// import NavBarSimple from "./components/NavBarSimple";
 import Sidebar from "./components/Sidebar";
-import Content from "./components/Content";
+import NavBarForm from "./components/NavBarForm";
+// import Content from "./components/Content";
+// import ContentHooks from "./components/ContentHooks";
+// import ContentAPI from "./components/ContentAPI";
+import ContentAPIHooks from "./components/ContentAPIHooks";
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
-      <Content />
+      <NavBarForm />
       <Sidebar />
+      {/* <Content /> */}
+      {/* <ContentHooks /> */}
+      {/* <ContentAPI /> */}
+      <ContentAPIHooks />
     </div>
   );
 }
--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -3,7 +3,7 @@ import css from "./css/Sidebar.module.css";
 
 function Sidebar() {
     return (
-        <div className={CSS.Sidebar}>
+        <div className={css.Sidebar}>
             <a href = "#." target= "_blank">My Photos</a>
             <a href = "#." target= "_blank">My Illustrations</a>
             <a href = "#." target= "_blank">My Paintings</a>
